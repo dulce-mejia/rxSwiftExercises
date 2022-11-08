d99cb0123347cb7ee3534205742e6bf2e62b1b6b
Gardenia Mejia <dgma0607@gmail.com>
2022-11-08 16:05:26 -0600

    initial commit

---
+++ RxSwiftPlaygroundUITests/RxSwiftPlaygroundUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RxSwiftPlaygroundUITestsLaunchTests.swift
+//  RxSwiftPlaygroundUITests
+//
+//  Created by Gardenia Mejia on 08/11/22.
+//
+
+import XCTest
+
+final class RxSwiftPlaygroundUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
